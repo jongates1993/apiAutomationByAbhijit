008c65c577fb0914d539479b775cc9490e592cf2
Abhijit Surve <abhijitsurve1993@gmail.com>
2020-10-03 12:59:08 +0530

    added Validator class

--- src/test/java/com/api/test/apivalidation.java
+++ src/test/java/com/api/test/apivalidation.java
@@ -13,23 +13,24 @@ import org.testng.annotations.Test;
 import static io.restassured.RestAssured.get;
 import static io.restassured.path.json.JsonPath.given;
 import static org.hamcrest.Matchers.hasItems;
+import com.dev.apitest.validator;
 
 public class apivalidation {
 
     final static String URL = "http://dummy.restapiexample.com/api/v1/employees";
+    validator v = new validator();
 
     @Test
-    public static void getBody(){
+    public void getBody(){
 
         Response response = get(URL);
         System.out.println("Response from restapi is -----> " + response.asString());
-
+        v.validateResponse(response);
 //        response.then().body("employee_name", hasItems("imam", "50909"));
-
     }
 
-    public static void main(String args[]){
-        getBody();
-    }
+//    public static void main(String args[]){
+//        getBody();
+//    }
 }
 
